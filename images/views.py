1e40c58611a0ee7cc8ab42bec91946e2ff7b9dfb
Mary Xia <Mary Xia>
2016-04-23 04:42:36 +0100

    added nsfw image classification

--- images/views.py
+++ images/views.py
@@ -1,4 +1,5 @@
 import os
+import requests
 import json
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader # This loads the template for html files
@@ -35,38 +36,12 @@ def create(request):
     # os.environ["CLARIFAI_APP_ID"] = <id>
     # os.environ["CLARIFAI_APP_SECRET"] = <secret>
 
+    clarifai_api = ClarifaiApi()
     image = Images(date_added=timezone.now())
     try:
         image.img_source = request.POST['img_source']
-        # tags is a set containing tags from both the user input and the API
-        tags = set()
-        input_tags = request.POST['tags'].split(', ')
-        for tag in input_tags:
-            tags.add(tag)
-        try:
-            clarifai_api = ClarifaiApi()
-            image_tag_results = clarifai_api.tag_image_urls([image.img_source])
-        except (KeyError):
-            print "You don't have authorization to use the Clarifai API."
-            return render(request, 'images/index.html', {
-                'error_message': 'Error Accessing Clarifai API',
-            })
-        else:
-            # I only have 1 image so [0]
-            clarifai_tags = image_tag_results["results"][0]["result"]["tag"]["classes"]
-            # If still image, i.e. tag_results = [tag1, tag2]
-            if (type(clarifai_tags[0]) == str):
-                for tag in clarifai_tags:
-                    tags.add(tag)
-            else:
-            # gif, i.e. tag_results = [[tag1, tag2], [tag3, tag4]] for each frame
-                for frame in clarifai_tags:
-                    for tag in frame:
-                        tags.add(tag)
-
-            image.tags = ', '.join(list(tags))
-
-        # image.nsfw_probability = request.POST['nsfw_probability']
+        image.tags = get_tags(request.POST['tags'].split(', '), image.img_source, clarifai_api)
+        image.nsfw_probability = get_nsfw(image.img_source)
     except (KeyError, Images.DoesNotExist):
         return render(request, 'images/index.html', {
             'error_message': 'Error Creating Image',
@@ -80,7 +55,7 @@ def edit(request, image_id):
     try:
         image.img_source = request.POST['img_source']
         image.tags = request.POST['tags']
-        # image.nsfw_probability = request.POST['nsfw_probability']
+        image.nsfw_probability = get_nsfw(image.img_source)
     except (KeyError, Images.DoesNotExist):
         return render(request, 'images/detail.html', {
             'image': image,
@@ -92,4 +67,52 @@ def edit(request, image_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('images:detail', args=(image.id,)))
-    
+
+# tags is a set containing tags from both the user input and the API
+def get_tags(input_tags, img_source, clarifai_api):
+    tags = set()
+    for tag in input_tags:
+        tags.add(tag)
+    try:
+        image_tag_results = clarifai_api.tag_image_urls(img_source)
+    except (KeyError):
+        print "You don't have authorization to use the Clarifai API."
+        return render(request, 'images/index.html', {
+            'error_message': 'Error Accessing Clarifai API',
+        })
+    else:
+        # I only have 1 image so [0]
+        clarifai_tags = image_tag_results["results"][0]["result"]["tag"]["classes"]
+
+        # If still image, i.e. tag_results = [tag1, tag2]
+        if (type(clarifai_tags[0]) is unicode): # unicode is actually str
+            for tag in clarifai_tags:
+                tags.add(tag)
+        else:
+            # gif, i.e. tag_results = [[tag1, tag2], [tag3, tag4]] for each frame
+            for frame in clarifai_tags:
+                for tag in frame:
+                    tags.add(tag)
+
+        tags.remove('') # I dunno this gets added sometimes?
+        return ', '.join(list(tags))
+
+# curl "https://api.clarifai.com/v1/tag/?model=nsfw-v1.0&url=https://samples.clarifai.com/metro-north.jpg" -H "Authorization: Bearer {access_token}"
+def get_nsfw(img_source):
+    # Get your own access token
+    # access_token = <get your own>
+
+    nsfw = 0
+    try:
+        req = requests.get("https://api.clarifai.com/v1/tag/?model=nsfw-v1.0&url=" + img_source + "&access_token=" + access_token)
+        response = json.loads(req.text)
+        print response
+        tag_response = response["results"][0]["result"]["tag"]
+        if (tag_response["classes"][0] == 'nsfw'):
+            nsfw = tag_response["probs"][0] * 100
+        else:
+            nsfw = tag_response["probs"][1] * 100
+    except (KeyError):
+        print "Error in getting NSFW number??"
+    return nsfw
+
